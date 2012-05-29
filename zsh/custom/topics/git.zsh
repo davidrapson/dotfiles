7c8608d7517ed54ae7605e65a4dae25769231bb3
David Rapson <david@davidrapson.co.uk>
2012-05-29 20:05:20 +0100

    Add extra git aliases and functions

--- zsh/custom/topics/git.zsh
+++ zsh/custom/topics/git.zsh
@@ -1,13 +1,20 @@
 # Git
 # ===
 
+# Aliases
+# =======
+
 # More Git aliases in ~/.gitconfig
 # Inspired by Thoughtbot
 # http://robots.thoughtbot.com/post/4747482956/streamline-your-git-workflow-with-aliases
 # These extend the default oh-my-zsh git plugin
 
-alias gitbox='open -a /Applications/Gitbox.app .'
-alias ungit="find . -name '.git' -exec rm -rf {} \;"
+# Wrap git with hub
+hub_path=$(which hub)
+if [[ -f $hub_path ]]
+then
+  alias git=$hub_path
+fi
 
 # Some of these git aliases use the `git-smart` gem
 # `gem install git-smart` & `rbenv rehash`
@@ -19,17 +26,14 @@ alias gmr='git merge --ff-only'
 alias gup='git smart-pull'
 alias gomg='omglog'
 
+alias gitbox='open -a /Applications/Gitbox.app .'
+alias ungit="find . -name '.git' -exec rm -rf {} \;"
 
-export GIT_SANDBOX=~/.code/sandbox
 
-# Wrap git with hub
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
+# Functions
+# =========
 
-# sandbox user/repo
+# Sandbox user/repo
 function sandbox() {
-  cd $GIT_SANDBOX && git clone $1 && cd `last_modified`
+  cd ~/Developer/code/sandbox && git clone $1 && cd `last_modified`
 }
