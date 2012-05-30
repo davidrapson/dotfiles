208ee15f7f27005d9b0284299b6ab0ce379b53de
David Rapson <david@davidrapson.co.uk>
2012-05-30 11:09:01 +0100

    Add local and public IP functions

--- zsh/custom/topics/network.zsh
+++ zsh/custom/topics/network.zsh
@@ -15,7 +15,7 @@ alias flush="dscacheutil -flushcache"
 alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
-# One of @janmoesen’s ProTip™s
+# Add aliases for HTTP verbs
 for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
   alias "$method"="lwp-request -m '$method'"
 done
@@ -31,10 +31,22 @@ alias ps='ps -a -c -o pid,command -x'
 # Functions
 # =========
 
-# Show both local and remote IP addresses
+# Show both local and public IP addresses
 fuction network() {
   echo -e "Local  IP: $(ifconfig en1 | grep -oP 'inet \K(\d{1,3}\.){3}\d{1,3}')"
   echo -e "Public IP: $(curl -s icanhazip.com)";
+}
+
+# Get local IP and copy it to the clipboard
+function localip() {
+  ifconfig en1 | grep -oP 'inet \K(\d{1,3}\.){3}\d{1,3}' | tr -d '\n' | tee >(pbcopy)
+  echo '\nCopied to clipboard'
+}
+
+# Get public IP and copy it to the clipboard
+function publicip() {
+  curl -s icanhazip.com | tr -d '\n' | tee >(pbcopy)
+  echo '\nCopied to clipboard'
 }
 
 # All the dig info
@@ -42,6 +54,7 @@ function digga() {
   dig +nocmd "$1" any +multiline +noall +answer
 }
 
+# Just dig MX records
 function mx() {
   dig "$1" mx
 }
