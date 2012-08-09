8c92645bdf186085306449e131b743724a89958b
David Rapson <david.rapson@mademedia.co.uk>
2012-08-09 17:07:48 +0100

    Sort git aliases and add git smart-log alias

--- zsh/custom/topics/git.zsh
+++ zsh/custom/topics/git.zsh
@@ -19,14 +19,15 @@ fi
 
 # Some of these git aliases use the `git-smart` and `omglog` gems
 # `gem install git-smart omglog` then `rbenv rehash`
+alias gca='git commit --amend'
 alias gh='git browse'
+alias gl='git smart-log'
 alias gm='git smart-merge'
 alias gmr='git merge --ff-only'
 alias gomg='omglog'
 alias gp='git add --patch'
 alias gs='git status --branch --short'
 alias gup='git smart-pull'
-alias gca='git commit --amend'
 alias ungit="find . -name '.git' -exec rm -rf {} \;"
 
 
