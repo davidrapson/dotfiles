7ec913c97cfdfd824c4cc945b8cc8430ff0aab68
David Rapson <david@davidrapson.co.uk>
2013-05-12 15:17:59 +0100

    Merge android.zsh topic into dev.zsh

--- zsh/custom/topics/android.zsh
+++
@@ -1,9 +0,0 @@
-# Network
-# =======
-
-# Aliases
-# -------
-
-# Port forward Android devtools
-alias android_devtools='adb forward tcp:9222 localabstract:chrome_devtools_remote'
-
--- zsh/custom/topics/dev.zsh
+++ zsh/custom/topics/dev.zsh
@@ -4,6 +4,9 @@
 # Aliases
 # -------
 
+# Port forward Android devtools
+alias android_devtools='adb forward tcp:9222 localabstract:chrome_devtools_remote'
+
 # Functions
 # ---------
 
