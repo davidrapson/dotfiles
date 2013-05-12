32808c5ae4c7174ee9d226deb3890896a4980cf0
David Rapson <david@davidrapson.co.uk>
2013-05-12 15:18:26 +0100

    Add fixopenwith alias

--- zsh/custom/topics/helpers.zsh
+++ zsh/custom/topics/helpers.zsh
@@ -141,6 +141,11 @@ fi
 # Aliasing eachdir like this allows you to use aliases/functions as commands.
 alias eachdir=". eachdir"
 
+# Quick way to rebuild the Launch Services database and get rid
+# of duplicates in the Open With submenu.
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+
+
 # Functions
 # ---------
 
