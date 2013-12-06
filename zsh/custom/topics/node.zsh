9f0f9e3bfbf93b79d924edd9c7a10d755363aff9
David Rapson <david@davidrapson.co.uk>
2013-12-06 21:21:01 +0000

    Add npm-check-updates function

--- zsh/custom/topics/node.zsh
+++ zsh/custom/topics/node.zsh
@@ -8,3 +8,12 @@
 function gri() {
   npm install --save-dev grunt-"$@"
 }
+
+function npmui() {
+  if hash npm-check-updates 2>/dev/null; then
+    npm-check-updates -u && npm install
+  else
+    echo "\nYou need to install npm-check-updates first:\n"
+    echo "npm install npm-check-updates -g"
+  fi
+}
