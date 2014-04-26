79d6593ab2f223e3cc4a251e74001d994a084b62
David Rapson <david@davidrapson.co.uk>
2014-04-26 23:05:01 +0100

    Cleanup PATH

--- topics/system/_path.zsh
+++ topics/system/_path.zsh
@@ -1,15 +1,14 @@
-# Homebrew & rbenv enhanced path
+# Homebrew & rbenv enhanced PATH
 PATH="$HOME/Developer/bin:$HOME/Developer/sbin:$HOME/Developer/share/npm/bin:$HOME/.rbenv/bin:$PATH"; export PATH
 
-# Add coreutiles to path
+# Add coreutiles to PATH
 PATH="$HOME/Developer/opt/coreutils/libexec/gnubin:$PATH"; export PATH
 
-# Add dotfile binaries into the path
+# Add dotfile binaries to PATH
 PATH="$HOME/.dotfiles/bin:$PATH"; export PATH
 
 # For node, so modules are picked up
 NODE_PATH="$HOME/Developer/lib/node_modules"; export NODE_PATH
-export PHANTOMJS_BIN=$HOME/Developer/bin/phantomjs
 
-# Custom man pages path
+# Add custom man pages to PATH
 MANPATH="$HOME/Developer/share/man:$MANPATH"; export MANPATH
