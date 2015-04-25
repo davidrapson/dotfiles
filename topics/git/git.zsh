ba37e2803b0b9fe573a2999ade5e62c3cfe75f39
David Rapson <david@davidrapson.co.uk>
2015-04-25 18:21:39 +0100

    YAGNI, remove git-smart and aliases I've never used

--- topics/git/git.zsh
+++ topics/git/git.zsh
@@ -18,30 +18,15 @@ fi
 # More Git aliases in ~/.gitconfig
 # Inspired by Thoughtbot
 # http://robots.thoughtbot.com/post/4747482956/streamline-your-git-workflow-with-aliases
-# Some of these git aliases use the `git-smart` and `omglog` gems
-# `gem install git-smart omglog` then `rbenv rehash`
 alias gc='git commit'
 alias gco='git checkout'
 alias gau='git add --update'
 alias gca='git commit --amend'
-alias gh='git browse'
-alias gl='git smart-log'
-alias gm='git smart-merge'
-alias gmr='git merge --ff-only'
+alias gl='git log --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an %ar%Creset" --graph'
 alias gp='git add --patch'
 alias gcp='git cherry-pick'
 alias gst='git status --branch --short'
-alias gs='git status --branch --short'
 alias gra='git commit --amend --reset-author'
-alias gup='git smart-pull'
-alias gups='git smart-pull && git submodule sync && git submodule update --init'
-
-if (( $+commands[omglog] )) ; then
-    alias gomg='omglog'
-fi
-
-# Open any files marked as “modified” in default editor.
-alias gchanged='$EDITOR `git status --porcelain | sed -ne "s/^ M //p"`'
 
 # Ungit
 alias ungit="find . -name '.git' -exec rm -rf {} \;"
@@ -58,4 +43,3 @@ function current_branch() {
 alias ggpull='git pull origin $(current_branch)'
 alias ggpur='git pull --rebase origin $(current_branch)'
 alias ggpush='git push origin $(current_branch)'
-alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
