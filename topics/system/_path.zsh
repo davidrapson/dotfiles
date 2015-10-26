8e8fc2f15e8e4b8ddc198b798874b86f008093d8
David Rapson <david@davidrapson.co.uk>
2015-10-26 21:44:35 +0000

    Install homebrew to default /usr/local

--- topics/system/_path.zsh
+++ topics/system/_path.zsh
@@ -1,16 +1,20 @@
-# Homebrew & rbenv enhanced PATH
-PATH="/usr/local/bin:$PATH"; export PATH
+# Make sure /usr/local (Homebrew) comes first
+PATH="/usr/local/bin:/usr/local/sbin:$PATH"; export PATH
 
-PATH="$HOME/Developer/bin:$HOME/Developer/sbin:$HOME/Developer/share/npm/bin:$HOME/.rbenv/bin:$PATH"; export PATH
+# NPM & rbenv enhanced PATH
+PATH="/usr/local/share/npm/bin:$HOME/.rbenv/bin:$PATH"; export PATH
 
 # Add coreutiles to PATH
-PATH="$HOME/Developer/opt/coreutils/libexec/gnubin:$PATH"; export PATH
+PATH="/usr/local//opt/coreutils/libexec/gnubin:$PATH"; export PATH
 
 # Add dotfile binaries to PATH
 PATH="$HOME/.dotfiles/bin:$PATH"; export PATH
 
 # For node, so modules are picked up
-NODE_PATH="$HOME/Developer/lib/node_modules"; export NODE_PATH
+NODE_PATH="/usr/local/lib/node_modules"; export NODE_PATH
 
 # Add custom man pages to PATH
-MANPATH="$HOME/Developer/share/man:$MANPATH"; export MANPATH
+MANPATH="/usr/local/share/man:$MANPATH"; export MANPATH
+
+# Set RBENV_ROOT
+RBENV_ROOT="/usr/local/var/rbenv"; export RBENV_ROOT
