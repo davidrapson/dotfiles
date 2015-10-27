e4ca98fc86dba3e06969ad98848412924cee3891
David Rapson <david@davidrapson.co.uk>
2015-10-27 13:39:29 +0000

    Use brew --prefix for PATH

--- topics/system/_path.zsh
+++ topics/system/_path.zsh
@@ -1,20 +1,20 @@
 # Make sure /usr/local (Homebrew) comes first
-PATH="/usr/local/bin:/usr/local/sbin:$PATH"; export PATH
+PATH=/usr/local/bin:/usr/local/sbin:$PATH; export PATH
 
 # NPM & rbenv enhanced PATH
-PATH="/usr/local/share/npm/bin:$HOME/.rbenv/bin:$PATH"; export PATH
+PATH=$(brew --prefix)/share/npm/bin:$HOME/.rbenv/bin:$PATH; export PATH
 
 # Add coreutiles to PATH
-PATH="/usr/local//opt/coreutils/libexec/gnubin:$PATH"; export PATH
+PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH; export PATH
 
 # Add dotfile binaries to PATH
 PATH="$HOME/.dotfiles/bin:$PATH"; export PATH
 
 # For node, so modules are picked up
-NODE_PATH="/usr/local/lib/node_modules"; export NODE_PATH
+NODE_PATH="$(brew --prefix)/lib/node_modules"; export NODE_PATH
 
 # Add custom man pages to PATH
-MANPATH="/usr/local/share/man:$MANPATH"; export MANPATH
+MANPATH="$(brew --prefix)/share/man:$MANPATH"; export MANPATH
 
 # Set RBENV_ROOT
-RBENV_ROOT="/usr/local/var/rbenv"; export RBENV_ROOT
+RBENV_ROOT="$(brew --prefix)/var/rbenv"; export RBENV_ROOT
