6af41bf4f8d78c7db14b4d82770ad0ba9733e692
David Rapson <david.rapson@mademedia.co.uk>
2017-03-09 12:17:07 +0000

    Add PHP local server command

---
+++ topics/system/server.zsh
@@ -0,0 +1,20 @@
+# Server
+# ======
+
+# Start up a local server for the current directory.
+# Defaults to port 8000
+function local_server() {
+  open "http://localhost:${1:-8000}" && python -m SimpleHTTPServer ${1:-8000}
+}
+
+function local_server_php() {
+  php -S localhost:8000
+}
+
+# Start up a public server for the current directory using ngrok
+# brew install ngrok
+function public_server() {
+  python -m SimpleHTTPServer 8080 &
+  ngrok 8080
+}
+
