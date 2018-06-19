a17df8229130a72a7fc9468d16890343785ec328
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-06-19 11:50:31 +0100

    Remove npmu alias, npm outdated enough

--- topics/node/node.zsh
+++ topics/node/node.zsh
@@ -2,17 +2,7 @@
 # Node
 #######################################
 
-# Check for npm updates
-function npmu() {
-  if hash david 2>/dev/null; then
-    david
-  else
-    echo "\nYou need to install david first:\n"
-    echo "npm install david -g"
-  fi
-}
-
 # List globally installed dependencies (first level only)
 function npmg() {
-    npm list -g --depth 0
+  npm list -g --depth 0
 }
