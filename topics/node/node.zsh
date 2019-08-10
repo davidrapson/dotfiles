0607904dd98f2b1d5273d7acf0bf09de8bd89ba9
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-08-10 18:34:10 +0100

    Remove unused helpers

--- topics/node/node.zsh
+++
@@ -1,8 +0,0 @@
-#######################################
-# Node
-#######################################
-
-# List globally installed dependencies (first level only)
-function npmg() {
-  npm list -g --depth 0
-}
